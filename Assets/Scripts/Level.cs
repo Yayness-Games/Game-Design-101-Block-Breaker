b975d725afac96f27d3f96e6a7851a61aa08a82c
TodTheSushiMonster <69654470+TodTheSushiMonster@users.noreply.github.com>
2020-10-09 12:29:55 -0700

    Everything!
    Yay!

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+
+    //parameters
+    [SerializeField] int BreakableBlocks;  // Serialized for debugging purposes
+
+    // cached reference
+    SceneLoader sceneloader;
+
+    private void Start()
+    {
+        sceneloader = FindObjectOfType<SceneLoader>();
+    }
+
+    public void CountBlocks()
+    {
+        BreakableBlocks++;
+    }
+
+    public void BlockDestroyed()
+    {
+        BreakableBlocks--;
+        if (BreakableBlocks <= 0)
+        {
+            sceneloader.LoadNextScene();
+        }
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    // configuration paramaters
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float screenHeightInUnits = 12f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+    [SerializeField] float minY = 1f;
+    [SerializeField] float maxY = 11f;
+    [SerializeField] bool side;
+
+    // cached referances
+    GameSession theGameSession;
+    ball theBall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theGameSession = FindObjectOfType<GameSession>();
+        theBall = FindObjectOfType<ball>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        if (side)
+        {
+            paddlePos.y = Mathf.Clamp(GetYPos(), minY, maxY);
+        }
+        else
+        {
+            paddlePos.x = Mathf.Clamp(GetXPos(), minX, maxX);
+        }
+        transform.position = paddlePos;
+    }
+
+    private float GetXPos()
+    {
+        if (theGameSession.IsAutoPlayEnabled())
+        {
+            return theBall.transform.position.x;
+        }
+        else
+        {
+            return Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        }
+    }
+
+    private float GetYPos()
+    {
+        if (theGameSession.IsAutoPlayEnabled())
+        {
+            return theBall.transform.position.y;
+        }
+        else
+        {
+            return Input.mousePosition.y / Screen.height * screenHeightInUnits;
+        }
+    }
+}
